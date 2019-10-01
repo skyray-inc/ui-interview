998c0aa6c287577c6808a9e1f2b30f67958c784e
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-10-01 22:17:21 +0700

    Adding mock data and basic component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import mockData from "./mock-data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Logichub App</h1>
+      There are {mockData.events.length} events that needs to be listed
     </div>
   );
 }
