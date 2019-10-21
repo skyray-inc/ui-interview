dcf68db3ae72e068398819b7c1c777f715c32ac8
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-10-22 01:37:55 +0700

    Adding node-sass and change App to a stateful component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import "./App.scss";
 import mockData from "./mock-data";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Logichub App</h1>
-      There are {mockData.events.length} events that needs to be listed
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Logichub App</h1>
+        There are {mockData.events.length} events that needs to be listed
+      </div>
+    );
+  }
 }
 
 export default App;
